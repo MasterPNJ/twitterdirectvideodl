32a66f8a83890d1735e147c4e047df4d69dc198e
MasterPNJ <38404325+MasterPNJ@users.noreply.github.com>
2023-01-23 11:12:09 +0100

    Update twittersave.py

--- twittersave.py
+++ twittersave.py
@@ -35,11 +35,12 @@ api = tweepy.API(auth)
 #Generate a random character string for the video file
 
 def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
+    
     return ''.join(random.choice(chars) for _ in range(size))
 
 rande = id_generator()
 
-columns = ['Lien']
+columns = ['link']
 data = []
 
 class MyStream(tweepy.StreamingClient):
@@ -50,35 +51,53 @@ class MyStream(tweepy.StreamingClient):
     def on_tweet(self, tweet):
         
         if tweet.referenced_tweets == None:
+            
             rawtext = tweet.text
             # Extract all URLs with a regular expression
             matches = re.finditer(r"https?://\S+", rawtext)
+            
             for match in matches:
+                
                 url = match.group(0)
                 # Checking the validity of the URL with urlparse()
                 parsed_url = urlparse(url)
+                
                 if parsed_url.scheme and parsed_url.netloc:
+                    
                     print("Valid URL found in string:", url)
                     response = requests.get(url)
                     valid_url = response.url
                     print("Unwrapped URL:", valid_url)
+                    
                     if valid_url.find("photo") != -1:
+                        
                         print("the link is a picture")
+                        
                     else:
+                        
                         if valid_url.find("twitter.com") != -1:
+                            
                             # Creating the name of the destination folder
                             date_str = datetime.datetime.now().strftime("%Y-%m-%d")
                             # Creation of the destination folder path
                             output_dir = os.path.join(os.getcwd(), date_str)
+                            
                             # Creation of the file if it does not already exist
                             if not os.path.exists(output_dir):
+                                
                                 os.makedirs(output_dir)
+                                
                             # Update of the 'outtmpl' option for youtube-dl
                             ydl_opts = {'outtmpl': os.path.join(output_dir, id_generator() + '.mp4')}
+                            
                             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                                
                                 ydl.download([valid_url])
+                                
                             print("Download complete")
+                            
                         else:
+                            
                             print("Not a twitter link")
 
                             data.append([valid_url])
@@ -89,8 +108,11 @@ class MyStream(tweepy.StreamingClient):
                             df.to_csv(rande+'.csv')
                     
                 else:
+                    
                     print("Invalid URL found in string:", url)
+                    
             else:
+                
                 print("No URL found in string")
                 
             date = datetime.datetime.now()
@@ -102,24 +124,36 @@ class MyStream(tweepy.StreamingClient):
 search_terms = []
 
 def menu():
+    
     print("1. View saved search terms")
     print("2. Add new search terms")
     print("3. Delete all search terms")
     print("4. Launch the main program")
     choice = input("Make your choice: ")
+    
     if choice == "1":
+        
         display_terms()
+        
     elif choice == "2":
+        
         add_terms()
+        
     elif choice == "3":
+        
         delete_terms()
+        
     elif choice == "4":
+        
         main_program()
+        
     else:
+        
         print("Invalid choice, please try again.")
         menu()
 
 def display_terms():
+    
     stream = MyStream(bearer_token=bearer_token)
     list = []
     list = stream.get_rules() # get all rules
@@ -127,34 +161,44 @@ def display_terms():
     menu()
 
 def add_terms():
+    
     stream = MyStream(bearer_token=bearer_token)
     search_terms = []
     new_term = input("Enter the new search term: ")
     search_terms.append(new_term)
+    
     for term in search_terms:
+        
         stream.add_rules(tweepy.StreamRule(term))
+        
     print("Term successfully added.")
     menu()
 
 def delete_terms():
-    stream = MyStream(bearer_token=bearer_token)
     
+    stream = MyStream(bearer_token=bearer_token)
     list = []
     list = stream.get_rules() # get all rules
     
     try:
+        
         for delet in list:   
             stream.delete_rules(delet) # delete all rules
+            
     except tweepy.errors.BadRequest as e:
+        
         print("All rules have been successfully removed")
         menu()
 
 def main_program():
+    
     stream = MyStream(bearer_token=bearer_token)
     list = []
     list = stream.get_rules() # get all rules
     print(list)
+    
     for term in search_terms:
+        
         stream.add_rules(tweepy.StreamRule(term))
 
     stream.filter(tweet_fields=["referenced_tweets"])
